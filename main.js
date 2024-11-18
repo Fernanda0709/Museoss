2846e593f103e1d050bb23d23dbd422f1be7651e
Fernanda0709 <luisafgonzalez2004@gmail.com>
2024-11-17 21:38:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -169,7 +169,6 @@ const materialS = {
 
 loadFBXModel('Museo.fbx', { x:1 , y: 1, z: 1 }, { x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, materialS);
 
-
 // Grupo para representar al jugador
 const player = new THREE.Group();
 player.add(camera);
